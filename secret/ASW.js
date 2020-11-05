e75c42cf7c0ff6aaf6a90b301e04c1efdef3be4c
lambm <thanhlamcntt1@gmail.com>
2020-11-05 13:11:06 +0700

    as

--- secret/ASW.js
+++ secret/ASW.js
@@ -1,6 +1,6 @@
 module.exports={
     aws:{
-        "accessKeyId": 'secret.aws.AKIAITPQMQNEQPTOP7CQ', 
+        "accessKeyId": 'AKIAITPQMQNEQPTOP7CQ', 
         "secretAccessKey":'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' 
     }
 }
